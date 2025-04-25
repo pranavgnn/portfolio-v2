b9b7d72d9ba713f00e8acfa8072c5a3122f391ac
pranavgnn <pranavgnayak@gmail.com>
2025-04-25 18:56:24 +0530

    increased footer width

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Separator } from "./ui/separator";
 
 const Footer = () => {
   return (
-    <footer className="max-w-4xl mx-auto text-center text-sm space-y-1 pb-4">
+    <footer className="max-w-11/12 mx-auto text-center text-sm space-y-1 pb-4">
       <Separator className="mb-4" />
       <p className="hidden md:block">Enough scrolling. 😤</p>
       <p className="text-muted-foreground">
