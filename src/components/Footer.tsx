e6ba22a5173931415ab2cb12f72235e840ae8f67
pranavgnn <pranavgnayak@gmail.com>
2025-04-25 22:26:11 +0530

    simplified footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,15 +4,13 @@ const Footer = () => {
   return (
     <footer className="max-w-11/12 mx-auto text-center text-sm space-y-1 pb-4">
       <Separator className="mb-4" />
-      <p className="hidden md:block">Enough scrolling. 😤</p>
+      <p>Enough scrolling. 😤</p>
       <p className="text-muted-foreground">
         <span>Made with </span>
         <span className="font-semibold text-destructive absolute hover:-rotate-45 hover:scale-105 duration-200">
           {"</3"}
         </span>
         <span className="ml-7">by pranav</span>
-        <br />
-        Created on 23rd April 2025. Updated on 25th April 2025.
       </p>
     </footer>
   );
